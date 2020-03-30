29c6a20cd226898f8d90b71581bd311de8b9d58f
irk\drilon.jahiri <irk\drilon.jahiri>
2020-03-30 20:56:57 +0200

    Create SignUp Form and Validate Form

--- TheHouseOfDelightV1/Controllers/AccountController.cs
+++ TheHouseOfDelightV1/Controllers/AccountController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TheHouseOfDelightV1.DbModel;
+using TheHouseOfDelightV1.Models;
 
 namespace TheHouseOfDelightV1.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly ApplicationDbContext dbContext;
+        public AccountController(ApplicationDbContext dbC)
+        {
+            this.dbContext = dbC;
+        }
         public IActionResult LogIn()
         {
             return View();
@@ -18,6 +27,25 @@ namespace TheHouseOfDelightV1.Controllers
         {
             
             return Redirect("/Account/LogIn");
+        }
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(Users model)
+        {
+            var SignUp = new Users();
+            SignUp.Name = model.Name;
+            SignUp.Surname = model.Surname;
+            SignUp.Email = model.Email;
+            SignUp.Password = model.Password;
+            SignUp.City = model.City;
+            dbContext.Users.Add(SignUp);
+            dbContext.SaveChanges();
+            return Redirect("/Account/SignUp");
         }
     }
 }
